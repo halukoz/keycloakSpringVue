8193d270f5238b2434ce0bbd896e8b41c78b3392
Özduman <Haluk.Oezduman@opitz-consulting.com>
2023-10-11 10:25:45 +0200

    first commit

---
+++ src/main/java/com/demoproject/keycloak/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.demoproject.keycloak;
+
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+            http
+                    .csrf(Customizer.withDefaults())
+                    .authorizeHttpRequests(authorize -> authorize
+                            .anyRequest().authenticated()
+                    )
+                    .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                    .httpBasic(Customizer.withDefaults())
+                    .formLogin(Customizer.withDefaults());
+            return http.build();
+    }
+
+}
